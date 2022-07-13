451ab81011814c9ff953be6226af4dbf7554677b
SHEN <1014628363@qq.com>
2022-07-13 09:37:59 +0800

    support DLS2

--- 1.hello-world/2.read-and-parse-drivers-license-react/src/dcp.js
+++ 1.hello-world/2.read-and-parse-drivers-license-react/src/dcp.js
@@ -1,14 +1,17 @@
 import { CodeParser } from 'shen-dynamsoft-code-parser';
 
+// todo: should remove when publish
+CodeParser.licenseServer = "https://mtpl.dynamsoft.com";
+
 /** LICENSE ALERT - README
  * To use the library, you need to first specify a license key using the API "license" as shown below.
  */
 
- CodeParser.license = 't0068lQAAAKNd+QBe1NZs2uSAhM92occ3jqv0v1XiM7nJzJp1rYvg+0RnAFfX071kepLy4JZXVsyc6qRtu6LZUYojXxNfrhk=';
+ CodeParser.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMjM0ODEwLVRYbFhaV0pRY205cVgyUmpjQSIsIm9yZ2FuaXphdGlvbklEIjoiMjM0ODEwIn0=';
 
 /**
  * You can contact Dynamsoft Support Team (https://www.dynamsoft.com/Company/Contact.aspx) to obtain a license.
  * LICENSE ALERT - THE END
  */
 
- CodeParser.engineResourcePath = 'https://cdn.jsdelivr.net/npm/shen-dynamsoft-code-parser@0.20220708163942.0/dist/';
+ CodeParser.engineResourcePath = 'https://cdn.jsdelivr.net/npm/shen-dynamsoft-code-parser@0.20220712174000.0/dist/';
